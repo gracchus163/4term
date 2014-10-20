3cfd81da35e2f30105fb705c36b6abdfe12a472b
matt <mcalder@unseen.is>
2014-10-20 22:09:09 +0100

    renaming repo

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-clang json.c 4ch.c 4curses.c -o program -lcurl -ljansson -lcurses -g
+clang json.c 4ch.c 4term.c -o program -lcurl -ljansson -ltermbox -g
 #gcc json.o 4ch.c 4curses.c -o program -lcurl -ljansson -lncurses -g
