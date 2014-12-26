045ebb3c325c76555a6671d9c7e003002e6355c5
matt <mcalder@unseen.is>
2014-12-26 00:42:06 +0000

    some cleaning

--- 4term.c
+++ 4term.c
@@ -4,6 +4,8 @@
 #include <termbox.h>
 #include "termutil.h"
 
+int FG = TB_DEFAULT;
+int BG = TB_BLACK;
 void print_window(struct window* w)
 {
 	int win_edge = w->ypos+w->height;
@@ -11,16 +13,16 @@ void print_window(struct window* w)
 	for(int y = w->ypos; y < win_edge && y<=rows; y ++)
 	{
 		for(int x = w->xpos; x < (int)strlen(*(w->rows+y)); x++)
-		{//garbage from blank lines.  maybe something wrong with quoting post numbers also
+		{//maybe something wrong with quoting post numbers also
 			if(*(*(w->rows+y+w->scrollpos)+x)==0) break;
-			tb_change_cell(x,y, *(*(w->rows+y+w->scrollpos)+x), TB_DEFAULT, TB_BLACK);
+			tb_change_cell(x,y, *(*(w->rows+y+w->scrollpos)+x), FG, BG);
 		}
 	}
 
 }
 void write_window(struct window* win, struct thread_data* thrd)
 {
-	int w = win->width; int h = win->height;
+	int w = win->width;
 	char* title = malloc(sizeof(char)*w+1);
 	snprintf(title, w,"No.%d - %s", thrd->no, thrd->subject); 
 	char** r = win->rows;
@@ -45,7 +47,7 @@ for(int x = 0; x < w; x++)
 	//the sliding window over it
 	//height of window and height of screen are different.
 	//redo the outer loop, shouldn't stop until thrd_len is reached
-	for(int y =1; 1; y++)
+	for(int y =1; n<=thrd_len; y++)
 	{
 		*(r+y) = malloc(sizeof(char)*w);
 		win->no_rows += 1;
@@ -71,57 +73,6 @@ for(int x = 0; x < w; x++)
 	}
 	return;
 }
-void print_post(char* str)
-{
-	for(int y = 0; y < tb_height(); y++)
-	{
-		for(int x = 0; x < tb_width(); x++)
-		{
-			tb_change_cell(x,y, *(str++), TB_DEFAULT, TB_BLACK);
-		
-		}
-	}
-}
-int print_thread(struct thread_data thrd)
-{//loops through screen, printing lines from post until reaching the bottom
-	//modify to print to window struct and then have a print_window func
-	int w = tb_width(); int h = tb_height();
-	char* title = (char*)malloc(sizeof(char)*w+1);
-	snprintf(title, w,"No.%d - %s", thrd.no, thrd.subject); 
-for(int x = 0; x < w; x++) 
-{
-	if(*title == '\0')
-		tb_change_cell(x, 0, '=', TB_DEFAULT, TB_BLACK);
-	else
-		tb_change_cell(x,0, *(title++), TB_DEFAULT, TB_BLACK);
-}
-
-	int n = 0; int thrd_len = thrd.replies;
-	struct post_data p = thrd.posts[n];
-	char* com = p.post;
-	for(int y =1; y < h; y++)
-	{
-		for(int x = 0; x < w; x++)
-		{
-			if(*com == '\0')
-			{
-				//free(com);
-				if(n++ >= thrd_len) return 1;
-				p = thrd.posts[n];
-				com = p.post;
-				break;
-			}
-			if(*com == '\n')
-			{
-				++com;
-				break;
-			}
-			tb_change_cell(x,y, *(com++), TB_DEFAULT, TB_BLACK);
-		}
-	}
-	//free(com);
-	return 0;
-}
 int main()
 {
 	int thread_id;
@@ -139,7 +90,7 @@ int main()
 		return 1;
 	}
 	tb_init();
-	tb_set_clear_attributes(TB_DEFAULT, TB_BLACK);
+	tb_set_clear_attributes(FG,BG);
 	tb_clear();
 
 	get_thread_data(data, &thrd);
